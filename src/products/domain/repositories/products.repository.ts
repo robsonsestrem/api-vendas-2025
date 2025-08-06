466f0406fbd74ccf64d1cbeb8c2089009ed7bd3a
Robson Sestrem <sestrem.robson@gmail.com>
2025-08-05 21:53:11 -0300

    * implement ProductsInMemoryRepository with methods for finding products and handling name conflicts;

---
+++ src/products/domain/repositories/products.repository.ts
@@ -0,0 +1,22 @@
+import { RepositoryInterface } from '@/common/domain/repositories/repository.interface'
+import { ProductModel } from '../models/products-model'
+
+export type CreateProductProps = {
+	id: string
+	name: string
+	price: number
+	quantity: number
+	created_at: Date
+	updated_at: Date
+}
+
+export type ProductId = {
+	id: string
+}
+
+export interface ProductsRepository
+	extends RepositoryInterface<ProductModel, CreateProductProps> {
+	findByName(name: string): Promise<ProductModel>
+	findAllByIds(productIds: ProductId[]): Promise<ProductModel[]>
+	conflictingName(name: string): Promise<void>
+}
